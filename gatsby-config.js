aead488979915976ac74afda9c2db4904eb485b7
NorbertLuszkiewicz <norbertluszkiewicz00@gmail.com>
2020-11-26 12:34:30 +0100

    Update Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,28 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Hatta`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@norbertluszkiewicz`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-layout`,
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-webfonts`,
       options: {
-        fonts: [
-          {
-            family: `Montserrat`,
-            variants: [`400`, "600", `700`],
-          },
-          {
-            family: `Open Sans`,
-            variants: [`400`, "600", `700`],
-          },
-        ],
+        fonts: {
+          google: [
+            {
+              family: `Montserrat`,
+              variants: [`400`, "600", `700`],
+            },
+            {
+              family: `Open Sans`,
+              variants: [`400`, "600", `700`],
+            },
+          ],
+        },
       },
     },
     {
@@ -51,8 +53,5 @@ module.exports = {
         display: `minimal-ui`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
